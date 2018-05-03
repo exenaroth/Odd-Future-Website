37dbadfdaeda6618b562cd1063f4f4a479794ae0
exenaroth <baileyshaw0805@gmail.com>
2018-05-03 14:33:00 +0100

    Adding band page along with JS for a tab system

--- JS/script.js
+++ JS/script.js
@@ -7,6 +7,17 @@ $(document).ready(function($) {
       $(this).next().slideToggle('fast');  
 
     });
+
+     $('#accordionA').find('#tyler').click(function(){
+
+      //Expand or collapse this panel
+      $(this).next().slideToggle('fast');  
+
+    });
+
+     /*this is to make sure that the video size changes depending on the size of the scren*/
+    $('.iFrame').css('height', $(window).height()-'px');
+
+    
   });
 
-$('.iFrame').css('height', $(window).height()-'px');
